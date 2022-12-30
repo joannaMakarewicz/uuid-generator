6cf2f35df5567741f547dd66ed904619ba658827
Joanna Makarewicz <joanna.milun@gmail.com>
2022-12-30 15:36:33 +0100

    uidd library&unction for generation added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import { v4 as uuidv4 } from "uuid";
+
+let myuuid = uuidv4();
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello world</p>
-    </div>
+      <div className="App">
+        {myuuid},
+        <a href="index.html">Odśwież stronę</a>
+      </div>
   );
 }
 
