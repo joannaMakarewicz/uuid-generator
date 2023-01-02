e0b7b0111363e7ffaba521c11c7c59471fdd0d06
Joanna Makarewicz <joanna.milun@gmail.com>
2023-01-02 10:20:01 +0100

    header&content area added

--- src/App.js
+++ src/App.js
@@ -6,12 +6,17 @@ let myuuid = uuidv4();
 function App() {
   return (
     <div className="App">
-      <div>
-      {myuuid}
+      <h1 className="Header">Online UUID Generator</h1>
+      <p className="Content">A universally unique identifier (UUID) is a 128-bit label used for information in computer systems. When generated according to the standard methods, UUIDs are unique. </p>
+      <div className="Frame">
+        <div>
+          {myuuid}
+        </div>
+        <p>
+          W celu wygenerowania nowego kodu{" "}
+          <a href="index.html">odśwież stronę</a>
+        </p>
       </div>
-      <p>
-        W celu wygenerowania nowego kodu <a href="index.html">odśwież stronę</a>
-      </p>
     </div>
   );
 }
